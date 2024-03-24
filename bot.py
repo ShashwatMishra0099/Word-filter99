0e48ddb14bbe5ac0d4d988db16d4c26f451de54b
ShashwatMishra0099 <163753719+ShashwatMishra0099@users.noreply.github.com>
2024-03-24 11:18:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ TOKEN = '7110027433:AAGsM3zFlvER09jVq2ACTKYCfghoqAMZs_s'
 bot = telebot.TeleBot(TOKEN)
 
 # List of filtered words
-filtered_words = ["bad", "inappropriate", "spam", "/banall"]
+filtered_words = ["bad", "inappropriate", "spam", "fuck u"]
 
 @bot.message_handler(func=lambda message: True)
 def filter_message(message):
